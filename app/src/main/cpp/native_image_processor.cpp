4c50a5cb51cf78a56d09bb4d1a9213a68d22d2c4
amalshiwantha <amal@soho.com.au>
2026-09-25 10:41:32 +0530

    Implement `sayHello` JNI function in `native_image_processor.cpp`

---
+++ app/src/main/cpp/native_image_processor.cpp
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include <android/log.h>
+
+#define LOG_TAG "NativeImageProcessor"
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_nativeimageprocessor_NativeImageProcessor_sayHello(
+        JNIEnv* env,
+        jobject thiz) {
+
+    __android_log_print(
+            ANDROID_LOG_INFO,
+            LOG_TAG,
+            "Hello from C++!"
+    );
+}
+
